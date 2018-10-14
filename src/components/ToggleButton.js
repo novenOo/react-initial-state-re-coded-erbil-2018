52e5c04530362d2984adc37913dc9c76dd0c664d
noven temer <noven5823@gmail.com>
2018-10-14 08:10:25 +0000

    automatically backed up by learn

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,4 +1,17 @@
 // Component Code Goes Here
 import React from 'react';
 
-class ToggleButton extends React.Component
+class ToggleButton extends React.Component{
+  constructor(){
+    super();
+    this.state ={
+      isEnabled: false
+    }
+  }
+  render(){
+    return(
+      <button className='Toggle-button'>
+      I am toggled {this.state.isEnabled? 'on' : 'off'}</button>
+      )
+  }
+}
