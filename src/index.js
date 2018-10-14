2ede30bbe88a770ba4df72d56dd94e79b722176a
noven temer <noven5823@gmail.com>
2018-10-14 08:19:12 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ToggleButton from './components/ToggleButton';
+import Addess from './components/Addess';
 
 ReactDOM.render(
   <div>Components Need To Be Mounted</div>,
